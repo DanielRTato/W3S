5f69c15e9d6d8873941a255f9c512fb00548b1c3
DanielRTato <drodrigueztato@danielcastelao.org>
2024-10-06 14:37:19 +0200

    crear una variable nueva juntando otras

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hola Mundo");
+        System.out.println("Aprendiendo Java");
+        System.out.println("Nice!");
+        System.out.print("Hola mundo2 "); // Dejé un espacio para poder leerlo mejor
+        System.out.println("Esto va en la misma linea");
+        System.out.println(50000); // los numeros no van dentro de comillas
+        /* comentarios
+        en varias lineas  */
+        /*
+        System.out.println(3 + 3); //suma
+        System.out.println(2 * 5); //multiplicacion
+        System.out.println (10 % 3); // da el resto
+        System.out.println(10 / 2); //division
+
+        String nombre = "Daniel";  //letras llevan comillas
+                System.out.println(nombre);   // pero los numeros no
+        int edad = 27;
+                System.out.println(edad);
+        int myNum;
+        myNum = 10;
+        myNum = 11; // Si pones un nuevo valor se sobreescribe
+                System.out.println(myNum);
+         */
+
+        String nombre = "Dani ";
+        String apellido = "Tato";
+        String nombreCompleto = nombre + apellido;
+        System.out.println("Hola " + nombre);
+        System.out.println(nombre + apellido);
+        System.out.println(nombreCompleto);
+
+        int x = 5;
+        int y = 6;
+        System.out.println(x + y);
+
+
+
+
+
+
+
+
+
+
+
+
+         }
+    }
